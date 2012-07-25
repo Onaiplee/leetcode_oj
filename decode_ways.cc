aeefecba5ffc70d79e26910d9474bec6a43820cc
Shaobi Li <shaobili@gmail.com>
2012-07-25 11:17:28 -0700

    modified the decode_ways.cc

--- decode_ways.cc
+++ decode_ways.cc
@@ -20,17 +20,26 @@
  * Assume a PACMAN can solve a sub-problem very well, you just use this pacman to solve the
  * a bigger problem (to make a bigger PACMAN ^_^). And when the PACMAN grows, you are done done. 
  * 
+ * Firstly, let us think it small. Suppose we have a string "218", how can we solve it by hand? 
+ * Notice the string is ending with "18", so it can be splitted as either "2|18" or "21|8". If we
+ * can know the number of ways that "2" and "21" can be decoded, it's done. 
+ *
+ *              solution("218")  = solution("2") + solution("21")
+ * 
+ * The solution of substring "2" and "21" are trivial, respectively, 1 and 2. So the final solution
+ * is 3. So if we have a string "xxxxxxxx18", the solution is easy as long as we know the solution of
+ * "xxxxxxxx" and "xxxxxxxx1". Now it's the time to see the general situation:
+ *
  * Assume S is the string that need to be decoded, with length N.
  * Create A[0..N-1], Let A[i] is the number of ways the sub-string S[0..i] can be decoded.
  * So if you are at i, you want to know A[i], and there are two PACMANs have solved A[i-1] and
  * A[i-2]. The only thing you need to do is how to use A[i-1] and A[i-2] to calculate A[i].
- * Here it is.
- * Let v = S[i-1] * 10 + S[i]
+ * Here it is. Let v = S[i-1] * 10 + S[i]
  *
  *          /   0,               v = 0 or ( 27 < v < 99 and v mod 10 = 0) 
  *         /
  * A[i] = -     A[i-1],          1 < v < 9 or (27 < v < 99 and v mod 10 <> 0)            
- *         \  
+ *         \
  *          \   A[i-2],          v = 10 or v = 20
  *           \
  *            \ A[i-1] + A[i-2], otherwise
