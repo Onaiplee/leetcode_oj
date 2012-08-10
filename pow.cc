3da3fed9b787aac1ec28f8e8b1293e633c4d8bf3
Shaobi Li <shaobili@gmail.com>
2012-08-09 22:52:11 -0700

    add pow.cc

---
+++ pow.cc
@@ -0,0 +1,27 @@
+// Implement pow(x, n).
+
+/**
+ * I got a runtime error during judge large section but I don't 
+ * know why...
+ */
+
+class Solution {
+public:
+    double pow(double x, int n) {
+        double m;
+        if (n < 0) {
+            return 1 / pow(x, -n);
+        }
+        if (n == 0) {
+            return 1;
+        }
+        if (n % 2) {
+            return x * pow(x, n-1);
+        }
+        else {
+            // Why not return pow(x, n/2) * pow(x, n/2)? ^_^
+            m = pow(x, n/2);
+            return m * m;
+        }
+    }
+};
