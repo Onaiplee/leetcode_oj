d9f805b4839a91bbcd1716ae6fb94730ea176ef3
Shaobi Li <shaobili@gmail.com>
2012-09-12 12:46:20 -0700

    add subsets.cc

---
+++ subsets.cc
@@ -0,0 +1,42 @@
+// Given a set of distinct integers, S, return all possible subsets.
+// 
+// Note:
+// Elements in a subset must be in non-descending order.
+// The solution set must not contain duplicate subsets.
+// For example,
+// If S = [1,2,3], a solution is:
+// 
+// [
+//   [3],
+//   [1],
+//   [2],
+//   [1,2,3],
+//   [1,3],
+//   [2,3],
+//   [1,2],
+//   []
+// ]
+
+/**
+ * I will add more solutions later.
+ */
+
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        sort(S.begin(), S.end());
+        vector<vector<int> > result;
+        result.push_back(vector<int>());
+        int size = S.size();       
+        for (int i = 0; i < S.size(); i++) {
+            int size = result.size();
+            for (int j = 0; j < size; j++) {
+                vector<int> temp = result[j];
+                temp.push_back(S[i]);
+                result.push_back(temp);
+            }
+        }
+        return result;
+    }
+    
+};
