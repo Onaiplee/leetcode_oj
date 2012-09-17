a098f7a8c9cd07b54e18533e8c1c7cb6243196b3
Shaobi Li <shaobili@gmail.com>
2012-09-17 14:32:34 -0700

    add memoization to wildcard_matching.cc

--- wildcard_matching.cc
+++ wildcard_matching.cc
@@ -18,7 +18,8 @@
 // isMatch("aab", "c*a*b") → false
 
 /**
- * I will add memoization later.
+ * The memoization solution consumes too much memory to pass the large OJ.
+ * I will add a better solution later.
  */
 
 class Solution {
@@ -53,5 +54,58 @@ public:
             return true;
         }
         return false;
+    }
+};
+
+/**
+ * DP using memoization.
+ */
+
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        vector<vector<int> > table(strlen(s) + 1, vector<int>(strlen(p) + 1, -1));
+        return is_match(s, p, table, 0, 0)? true : false;
+    }
+
+    int is_match(const char *s, const char *p, vector<vector<int> > &table, int i, int j) {
+        if (table[i][j] != -1) {
+            return table[i][j];
+        }
+        if (s[i] == '\0') {
+            if (p[j] == '\0') {
+                table[i][j] = 1;
+                return 1;
+            }
+            else if (p[j] == '*') {
+                table[i][j] = is_match(s, p, table, i, j+1);
+                return table[i][j];
+            }
+            else {
+                table[i][j] = 0;
+                return 0;
+            }
+        }
+        if (make_a_parse(s[i], p[j])) {
+            if (p[j] == '*') {
+                table[i][j] = is_match(s, p, table, i, j+1) || is_match(s, p, table, i+1, j) || is_match(s, p, table, i+1, j+1);
+                return table[i][j];
+            }
+            else {
+                table[i][j] = is_match(s, p, table, i+1, j+1);
+                return table[i][j];
+            }
+        }
+        else {
+            table[i][j] = 0;
+            return 0;
+        }
+    }
+    
+    int make_a_parse(const char s, const char p) {
+        if (((p == '?'|| s == p) && s != '\0') || p == '*' ) {
+            return 1;
+        }
+        return 0;
     }
 };
