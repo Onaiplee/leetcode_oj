6f15fdfe0184e70ce8c7157ab65dd4896dc01e51
Shaobi Li <shaobili@gmail.com>
2012-11-26 18:05:18 -0800

    modified word_search.cc, but cannot pass the large test

--- word_search.cc
+++ word_search.cc
@@ -20,22 +20,35 @@
  * Incomplete solution yet!!
  */
 
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
 class Solution {
 public:
     bool exist(vector<vector<char> > &board, string word) {
         vector<vector<char> > mark = board;
         for (int i = 0; i < board.size(); i++) {
             for (int j = 0; j < board[0].size(); j++) {
-                search(board, i, j, word)
+                bool ret = search(board, i, j, word);
+                if (ret == true) {
+                    return true;
+                }
             }
         }
-        dfs();
+        return false;
     }
     
     bool search(vector<vector<char> > &board, int i, int j, string word) {
-        dfs(board);
+        vector<bool> row(board[0].size(), false);
+        vector<vector<bool> > mark(board.size(), row);
+        return dfs(board, mark, 0, word, i, j);
     }
-    void dfs(vector<vector<char> > &board, vector<vector<char> > &mark, int depth, string word) {
+    bool dfs(vector<vector<char> > &board, vector<vector<bool> > &mark, int depth, string word, int i, int j) {
+        int  delta_x[4] = {1, -1, 0, 0};
+        int  delta_y[4] = {0, 0, 1, -1};
         if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size()) {
             if (depth == word.size()) {
                 return true;
@@ -45,18 +58,21 @@ public:
             }
         }
         else {
+            if (mark[i][j] == true) {
+                return false;
+            }
             if (word[depth] == board[i][j]) {
                 if (depth == word.size() - 1) {
                     return true;
                 }
                 mark[i][j] = true;
-                if (dfs(board, i+1, j, word) || dfs(board, i-1, j, word) || dfs(board, i, j+1, word) || dfs(board, i, j-1, word)) {
-                    return true;
+                for (int k = 0; k < 4; k++) {
+                    if (dfs(board, mark, depth+1, word, i+delta_x[k], j+delta_y[k])) {
+                        return true;
+                    }
                 }
-                else {
-                    mark[i][j] = false;
-                    return false;
-                }
+                mark[i][j] = false;
+                return false;
             }
             else {
                 return false;
@@ -64,3 +80,36 @@ public:
         }
     }
 };
+
+int main() 
+{
+    vector<vector<char> > board;
+    vector<char> r1;
+    r1.push_back('A');
+    r1.push_back('B');
+    r1.push_back('C');
+    r1.push_back('E');
+    vector<char> r2;
+    r2.push_back('S');
+    r2.push_back('F');
+    r2.push_back('C');
+    r2.push_back('S');
+    vector<char> r3;
+    r3.push_back('A');
+    r3.push_back('D');
+    r3.push_back('E');
+    r3.push_back('E');
+    board.push_back(r1);
+    board.push_back(r2);
+    board.push_back(r3);
+    //string word("ABCB");
+    //string word("ABCCED");
+    string word("SEE");
+    Solution s;
+    if (s.exist(board, word)) {
+        cout << "exsit!" << endl;
+    }
+    else {
+        cout << "doesn't exist!" << endl;
+    }
+}
